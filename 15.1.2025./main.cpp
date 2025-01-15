82bbe1f6c76b0b6e38b9083550fceafcf4e4f99b
patrik2338 <patrikzinic18@gmail.com>
2025-01-15 11:22:37 +0100

    Create main.cpp

---
+++ 15.1.2025./main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int r;
+    char a1;
+    char a2;
+    printf("molimo uensite slovo:");
+    cin>>a1;
+    //printf("hvala unesite drugo slovo:");
+    //cin>>a2;
+    r=a1;
+    printf("%c = %i", a1, r);
+    return 0;
+}
